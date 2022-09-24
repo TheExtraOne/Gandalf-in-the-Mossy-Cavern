38bac3a43cb72527be95443f15edd92a35331698
TheExtraOne <katya_mihasewa@mail.ru>
2022-09-24 15:57:12 +0300

    refactor: remove some js code to another files and delete extra img

---
+++ class.js
@@ -0,0 +1,176 @@
+'use strict'
+
+class Wizzard {
+    constructor(stayRight, stayLeft, runRight, runLeft, score = 0) {
+        this.positionX = 100;
+        this.positionY = 100;
+        this.width = 65;
+        this.height = 135;
+        this.speedX = 0;
+        this.speedY = 0;
+        this.accelY = gandalfAccelY;
+        this.cadre = 0;
+        this.standRight = stayRight;
+        this.standLeft = stayLeft,
+        this.runRight = runRight,
+        this.runLeft = runLeft,
+        this.currentState = this.standRight;
+        this.hasPower = false;
+        this.blockMovement = false;
+        this.count = 0;
+        this.score = score;
+    }
+    drawNewPosition() {
+        if (this.positionY + this.height + this.speedY < canvas.height) {
+            this.speedY += this.accelY;
+        }
+
+        this.positionX += this.speedX;
+        this.positionY += this.speedY;
+
+        ctx.drawImage(this.currentState, 165 * this.cadre, 0, 165, 277, this.positionX, this.positionY, this.width, this.height);
+        this.cadre++;
+        if (this.cadre > 79) {
+            this.cadre = 0;
+        }
+    }
+}
+
+class Enemy {
+    constructor(imageSlime, width, height, cropwidth, cropHeight, x, y, speedX, limit, isGreen = true) {
+        this.positionX = x;
+        this.positionY = y;
+        this.speedX = speedX;
+        this.speedY = 0;
+        this.accelY = gandalfAccelY;
+        this.image = imageSlime;
+        this.width = width;
+        this.height = height;
+        this.cadre = 0;
+        this.limit = limit;
+        this.distance = 0;
+        this.cropwidth = cropwidth;
+        this.cropHeight = cropHeight;
+        this.isGreen = isGreen;
+    }
+    drawEnemy() {
+        if (this.positionY + this.height + this.speedY < canvas.height) {
+            this.speedY += this.accelY;
+        }
+
+        this.distance += this.speedX;
+        if(Math.abs(this.distance) > this.limit) {
+            this.distance = 0;
+            this.speedX = -this.speedX;
+        }
+
+        this.positionX += this.speedX;
+        this.positionY += this.speedY;
+
+        ctx.drawImage(this.image, this.cropwidth * this.cadre, 0, this.cropwidth, this.cropHeight, this.positionX, this.positionY, this.width, this.height);
+        this.cadre++;
+        if (this.cadre > 79) {
+            this.cadre = 0;
+        }
+    }
+}
+
+class Flower {
+    constructor(imageFlower, width, height, cropwidth, cropHeight, x, y, ring = false) {
+        this.positionX = x;
+        this.positionY = y;
+        this.speedY = 0;
+        this.accelY = gandalfAccelY;
+        this.image = imageFlower;
+        this.width = width;
+        this.height = height;
+        this.cadre = 0;
+        this.cropwidth = cropwidth;
+        this.cropHeight = cropHeight;
+        this.ring = ring;
+    }
+    drawFlower() {
+        if (this.positionY + this.height + this.speedY < canvas.height) {
+            this.speedY += this.accelY;
+        }
+
+        this.positionY += this.speedY;
+        if (!this.ring) {
+            ctx.drawImage(this.image, this.cropwidth * this.cadre, 0, this.cropwidth, this.cropHeight, this.positionX, this.positionY, this.width, this.height);
+            this.cadre++;
+            if (this.cadre > 79) {
+                this.cadre = 0;
+            }
+        } else {
+            ctx.drawImage(this.image, 0, 0, this.cropwidth, this.cropHeight, this.positionX, this.positionY, this.width, this.height);
+        }
+    }
+}
+
+class Background {
+    constructor(x, y, image, imageWigth, imageHeight, block = false) {
+        this.positionX = x;
+        this.positionY = y;
+        this.speedX = 0;
+        this.width = imageWigth;
+        this.height = imageHeight;
+        this.image = image;
+        this.block = block;
+    }
+    drawBackground() {  
+        this.positionX += this.speedX;
+        ctx.drawImage(this.image, this.positionX, this.positionY);
+    }
+}
+
+class FireBall {
+    constructor(x, y, spedX) {
+        this.positionX = x;
+        this.positionY = y;
+        this.spedX = spedX;
+        this.radius = 8;
+        this.color = '#79d9c7';
+    }
+    draw() {
+        this.positionX += this.spedX;
+
+        ctx.beginPath();
+        ctx.arc(this.positionX, this.positionY, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+class Sphere {
+    constructor(x, y, speedX, speedY, radius, color, fireworks = false) {
+        this.positionX = x;
+        this.positionY = y;
+        this.spedX = speedX;
+        this.speedY = speedY;
+        this.radius = radius;
+        this.color = color;
+        this.accelY = gandalfAccelY;
+        this.isFireworks = fireworks;
+    }
+    draw() {
+        if (!this.isFireworks) {
+            if (this.positionY + this.radius + this.speedY < canvas.height) {
+                this.speedY += this.accelY * 0.1;
+            }
+        } else {
+            if (this.positionY + this.radius + this.speedY < canvas.height) {
+                this.speedY += this.accelY * 0.005;
+            }
+        }
+
+        this.positionX += this.spedX;
+        this.positionY += this.speedY;
+
+        ctx.beginPath();
+        ctx.arc(this.positionX, this.positionY, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+}
---
+++ sideFunctions.js
@@ -0,0 +1,100 @@
+'use strict'
+
+function lounchMusic(){
+    startButton.blur();
+    clickSoundInit(moneyAudio);
+    clickSoundInit(slimeAudio);
+    clickSoundInit(victoryAudio);
+    clickSoundInit(mainAudio);
+}
+
+function clickSoundInit(audio) {
+    if (audio === mainAudio) {
+        audio.play();
+        return;
+    }
+    audio.play(); // запускаем звук
+    audio.pause(); // и сразу останавливаем
+}
+
+function clickSound(audio) {
+    audio.currentTime = 0; // в секундах
+    audio.play();
+}
+
+//проверяю, находится ли игрок или его противник на платформе
+function doesToutchThePlatform({obj, platform}) {
+    return (obj.positionY + obj.height - howDeepInMoss <= platform.positionY &&
+        obj.positionY + obj.height + obj.speedY - howDeepInMoss >= platform.positionY &&
+        obj.positionX + obj.width - howCloseToPit >= platform.positionX &&
+        obj.positionX + howCloseToPit <= platform.positionX + platform.width);
+}
+//проверяю, запрыгнул ли герой на врага
+function doesHeroJumpOnTheEnemy({hero, enemy}) {
+    return (hero.positionY + hero.height <= enemy.positionY &&
+        hero.positionY + hero.height + hero.speedY  >= enemy.positionY &&
+        hero.positionX + hero.width - howCloseToPit >= enemy.positionX &&
+        hero.positionX + howCloseToPit <= enemy.positionX + enemy.width);
+}
+//проверка на соприкосновение с дном блока
+function doesHeroToutchTheBlock({hero, block}) {
+    return (hero.positionY < block.positionY + block.height &&
+        hero.positionX + hero.width > block.positionX &&
+        hero.positionX < block.positionX + block.width);
+}
+//проверка на соприкосновение со сторонами блока
+function doesHeroToutchTheSideOfBlock({hero, block}) {
+    return (hero.positionX + hero.width + hero.speedX - block.speedX > block.positionX &&
+        hero.positionX + hero.speedX < block.positionX + block.width &&
+        hero.positionY < block.positionY + block.height);
+}
+//проверка на соприкосновение со маной
+function doesHeroToutchMana({hero, mana}) {
+    return (hero.positionX + hero.width > mana.positionX &&
+        hero.positionX < mana.positionX + mana.width &&
+        hero.positionY + hero.height > mana.positionY &&
+        hero.positionY < mana.positionY + mana.height);
+}
+
+function updateScore(span1, span2, hero) {
+    span1.textContent = `mana-flowers: ${hero.count}`;
+    span2.textContent = `total score: ${hero.score}`;
+}
+
+function winStuff(hero, powerRing) {
+    hero.speedX = 0;
+    if (lastKey === 'KeyD') {
+        hero.currentState = hero.standRight;
+    } else {
+        hero.currentState = hero.standLeft;
+    }
+    isLeftPressed = false;
+    isRightPressed = false
+    hero.blockMovement = true;
+
+    clickSound(victoryAudio);
+
+    //салют, подсмотрено в интернете
+    for (let i = 0; i < 400; i++) {
+        spheres.push(new Sphere( (powerRing.positionX + powerRing.width / 2),
+        canvas.height / 3,
+        Math.cos(Math.PI * 2 * i/ 400) * Math.random(), 
+        Math.sin(Math.PI * 2 * i/ 400) * Math.random(), 
+        Math.random() * 3, 
+        `hsl(${randomDiap(1,100)}, 100%, 50%)`,
+        true));
+    }
+    
+    if (level === 2) {
+        return;
+    }
+
+    level++;
+    setTimeout(reset, 3000);
+}
+
+function randomDiap(n, m) {
+    return Math.floor(
+        Math.random() * (m - n + 1)
+    ) + n;
+}
