18870c61265361ae9007fce338c1982eddc1d606
TheExtraOne <katya_mihasewa@mail.ru>
2022-09-27 12:06:14 +0300

    feat:add folback function for animation frame

--- js/class.js
+++ js/class.js
@@ -69,7 +69,7 @@ class Enemy {
 
         ctx.drawImage(this.image, this.cropwidth * this.cadre, 0, this.cropwidth, this.cropHeight, this.positionX, this.positionY, this.width, this.height);
         this.cadre++;
-        if (this.cadre > 70) {
+        if (this.cadre > 79) {
             this.cadre = 0;
         }
     }
